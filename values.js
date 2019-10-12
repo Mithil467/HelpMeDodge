62c7b246f5e8dfd749c7458c514bcde459ff7de5
naidukarthi2193 <naidukarthi2193@gmail.com>
2019-10-12 23:47:05 +0530

    Upgraded the Game

--- values.js
+++ values.js
@@ -1,17 +1,17 @@
-var PipeColor = "yellow";
-var PlayerColor = "pink";
+var PipeColor = "red";
+var PlayerColor = "red";
 
 var BirdSpeed = 1;
 var PipeSpeed = 1;
 var PipeGap = 50;
 
-var UpKey = "ArrowLeft";
-var DownKey = "ArrowRight";
+var UpKey = "ArrowUp";
+var DownKey = "ArrowDown";
 
 var MadeByName = "Mithil";
 var Message = "DEAD!";
 var GameName = "Help Me DODGE viper!";
-var DisplayScore = false;
+var DisplayScore = true;
 
 
 /*

fed51f12c57bd132d0a7941509e305a2c6d3710d
RitiKarayat <ritikkarayat1@gmail.com>
2019-10-12 12:53:13 +0530

    Its Version 2.0 by RiTK

--- values.js
+++ values.js
@@ -1,17 +1,17 @@
-var PipeColor = "yellow";
-var PlayerColor = "pink";
+var PipeColor = "blue";
+var PlayerColor = "yellow";
 
-var BirdSpeed = 1;
-var PipeSpeed = 1;
+var BirdSpeed = 3;
+var PipeSpeed = 3;
 var PipeGap = 50;
 
-var UpKey = "ArrowLeft";
-var DownKey = "ArrowRight";
+var UpKey = "ArrowUp";
+var DownKey = "ArrowDown";
 
-var MadeByName = "Mithil";
-var Message = "DEAD!";
-var GameName = "Help Me DODGE viper!";
-var DisplayScore = false;
+var MadeByName = "Ritik";
+var Message = "You Ded Nigga..^_^";
+var GameName = "Garibo Ka Flappy Bird!";
+var DisplayScore = true;
 
 
 /*

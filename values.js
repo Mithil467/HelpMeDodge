a50bf58b5e3b914a572eff6fd31ed592e1c5bcad
Anu <anushree.kolhe18@siesgst.ac.in>
2019-10-12 13:02:31 +0530

    Change of vars

--- values.js
+++ values.js
@@ -1,17 +1,17 @@
-var PipeColor = "yellow";
-var PlayerColor = "pink";
+var PipeColor = "black";
+var PlayerColor = "red";
 
-var BirdSpeed = 1;
-var PipeSpeed = 1;
+var BirdSpeed = 2;
+var PipeSpeed = 3;
 var PipeGap = 50;
 
-var UpKey = "ArrowLeft";
-var DownKey = "ArrowRight";
+var UpKey = "ArrowUp";
+var DownKey = "ArrowDown";
 
 var MadeByName = "Mithil";
 var Message = "DEAD!";
 var GameName = "Help Me DODGE viper!";
-var DisplayScore = false;
+var DisplayScore = true;
 
 
 /*
